134adbcd0ddd62298e78a9b00f1bc12ec6dd1935
WandyYing <wandy1208@gmail.com>
2014-05-12 15:59:49 +0800

    Update __init__.py

--- csv2Sqlite/__init__.py
+++ csv2Sqlite/__init__.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2010 Wandy Ying
+#  Copyright (c) 2014 Wandy Ying
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
